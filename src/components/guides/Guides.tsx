b2df2947f30c00b594f23f0b32d0a85dd0fa4d53
Cameron Omiccioli <cameron.omiccioli@workhuman.com>
2023-07-12 13:55:42 -0400

    some work on the guides, not working though

--- src/components/guides/Guides.tsx
+++ src/components/guides/Guides.tsx
@@ -1,28 +1,20 @@
-import React from 'react';
-import { Box, Flex, SectionHeading } from '@contentful/f36-components';
-import { useAsync } from 'react-async-hook';
-import { useCMA, useSDK } from '@contentful/react-apps-toolkit';
-import { NavList } from '@contentful/f36-navlist';
+import React from "react";
+import { Box, Flex, SectionHeading } from "@contentful/f36-components";
+import { useAsync } from "react-async-hook";
+import { useCMA, useSDK } from "@contentful/react-apps-toolkit";
+import { NavList } from "@contentful/f36-navlist";
+import { url } from "inspector";
 
-const getGuides = async (cma: ReturnType<typeof useCMA>) => {
+const getGuides = async (
+  cma: ReturnType<typeof useCMA>,
+  sdk: ReturnType<typeof useSDK>
+) => {
+  console.log(cma, sdk);
   try {
-    const [contentTypes, entries, assets, locales, tags, users] =
-      await Promise.all([
-        cma.contentType.getMany({}),
-        cma.entry.getMany({}),
-        cma.asset.getMany({}),
-        cma.locale.getMany({}),
-        cma.tag.getMany({}),
-        cma.user.getManyForSpace({}),
-      ]);
-
+    const [guides] = sdk.parameters.installation.inputList;
+    console.log(guides);
     return {
-      contentTypes: { items: contentTypes.items, text: 'Content Types' },
-      entries: { num: entries.total, text: 'Entries' },
-      assets: { num: assets.total, text: 'Assets' },
-      locales: { num: locales.total, text: 'Locales' },
-      tags: { num: tags.total, text: 'Tags' },
-      users: { num: users.total, text: 'Users' },
+      items: guides,
     };
   } catch (e) {
     console.log(e);
@@ -33,49 +25,26 @@ const Guides = () => {
   const cma = useCMA();
   const sdk = useSDK();
 
-  console.log('cma', cma);
-  console.log('sdk', sdk);
+  console.log("cma", cma);
+  console.log("sdk", sdk);
 
-  console.log('sdk params', sdk.parameters.installation);
+  console.log("sdk params", sdk.parameters.installation);
 
-  const { result, loading } = useAsync(getGuides, [cma]);
+  const { result, loading } = useAsync(getGuides, [cma, sdk]);
+
+  // const items = sdk.parameters.installation.inputList?.map(
+  //   ([key, item]: [string, any]) => <div key={key}>{item}</div>
+  // );
 
   return (
     <Box>
       <SectionHeading>Guides:</SectionHeading>
       {loading ? (
-        <Flex marginTop="spacingXl">{/* <LoadingStats /> */}</Flex>
+        <Flex marginTop="spacingXl">Loading...</Flex>
       ) : (
         <NavList aria-label="Content Type Sidebar">
-          {result &&
-            Object.entries(result.contentTypes.items)
-              .filter(([key, value]) =>
-                // value.sys.id === 'blogPost' ||
-                // value.sys.id === 'page' ||
-                // value.sys.id === 'caseStudy' ||
-                // value.sys.id === 'document' ||
-                // value.sys.id === 'event' ||
-                // value.sys.id === 'newsArticle' ||
-                // value.sys.id === 'webinar' ||
-                sdk.parameters.installation.selectedSidebarCTs.includes(
-                  value.sys.id
-                )
-              )
-              .map(([key, value]: [string, any]) => (
-                <NavList.Item
-                  as={'button'}
-                  style={{
-                    justifyContent: 'flex-start',
-                    textDecoration: 'none',
-                  }}
-                  onClick={() => {
-                    sdk.navigator.openNewEntry(value.sys.id);
-                  }}
-                  key={key}
-                >
-                  ➕ Create a new {value.name}
-                </NavList.Item>
-              ))}
+          {/* <>{console.log(sdk.parameters.installation.inputList)}</> */}
+          {/* {items} */}
         </NavList>
       )}
     </Box>
--- src/locations/ConfigScreen.tsx
+++ src/locations/ConfigScreen.tsx
@@ -1,14 +1,40 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { ConfigAppSDK } from '@contentful/app-sdk';
-import { Heading, Form, Flex, Checkbox } from '@contentful/f36-components';
+import {
+  Heading,
+  Form,
+  Flex,
+  Checkbox,
+  Button,
+  Card,
+  Paragraph,
+  Subheading,
+  Stack,
+  TextInput,
+  FormControl,
+} from '@contentful/f36-components';
 import { useCMA, useSDK } from '@contentful/react-apps-toolkit';
 import { ContentTypeProps } from 'contentful-management';
 import tokens from '@contentful/f36-tokens';
+import GuideEditor from '../components/guides/GuideEditor';
 
-export type AppInstallationParameters = {
-  selectedSidebarCTs?: AppInstallationParameters | null | string[];
-  parameters?: AppInstallationParameters;
-};
+export interface Guide {
+  id: string;
+  name: string;
+  url: string;
+}
+
+export interface GuideList {
+  id: string;
+  name: string;
+  guideList: Guide[];
+}
+
+export interface AppInstallationParameters {
+  guidesList: GuideList[];
+  selectedSidebarCTs?: string[];
+}
+
 const onCTSelect = (
   selectedCTs: string[],
   setSelectedCTs: (cts: string[]) => void,
@@ -20,7 +46,8 @@ const onCTSelect = (
 };
 
 const ConfigScreen = () => {
-  const [parameters, setParameters] = useState<AppInstallationParameters>({});
+  const [inputList, setInputList] = useState<Array<any>>([]);
+
   const [contentTypes, setContentTypes] = useState<ContentTypeProps[]>([]);
   const [selectedSidebarCTs, setSelectedSidebarCTs] = useState<string[]>([]);
 
@@ -38,8 +65,8 @@ const ConfigScreen = () => {
         setSelectedSidebarCTs(currentParameters.selectedSidebarCTs);
       }
 
-      if (currentParameters) {
-        setParameters(currentParameters);
+      if (currentParameters?.inputList) {
+        setInputList(currentParameters.inputList);
       }
 
       // Once preparation has finished, call `setReady` to hide
@@ -48,22 +75,43 @@ const ConfigScreen = () => {
     })();
   }, [sdk]);
 
+  // handle input change
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    index: number
+  ) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index: number) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { name: '', url: '' }]);
+  };
+
   const onConfigure = useCallback(async () => {
     // This method will be called when a user clicks on "Install"
     // or "Save" in the configuration screen.
     // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
 
-    const updatedParameters: AppInstallationParameters = {
-      selectedSidebarCTs,
-      parameters,
-    };
+    // const updatedParameters: AppInstallationParameters =
 
     return {
-      parameters: updatedParameters,
+      parameters: {
+        selectedSidebarCTs,
+        inputList,
+      },
     };
-  }, [parameters, sdk, selectedSidebarCTs]);
-
-  console.log('line 122, parameters', parameters);
+  }, [sdk, selectedSidebarCTs, inputList]);
 
   const onSidebarContentTypeClick = useCallback(
     (ct: string) => {
@@ -125,6 +173,53 @@ const ConfigScreen = () => {
           ))}
         </Form>
         <Heading>Add Guides</Heading>
+        <Card>
+          <Stack flexDirection="column">
+            <>
+              {console.log(
+                'sdk.parameters.installation',
+                sdk.parameters.installation
+              )}
+            </>
+            {inputList.map((x, i) => {
+              return (
+                <Stack fullWidth>
+                  <FormControl isRequired>
+                    <FormControl.Label>Guide Name</FormControl.Label>
+                    <TextInput
+                      name="name"
+                      placeholder="name"
+                      value={x.name}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormControl.Label>Guide URL</FormControl.Label>
+                    <TextInput
+                      name="url"
+                      placeholder="url"
+                      value={x.url}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                  </FormControl>
+                  <Flex className="btn-box">
+                    {inputList.length !== 0 && (
+                      <Button
+                        variant="negative"
+                        onClick={() => handleRemoveClick(i)}
+                      >
+                        Remove
+                      </Button>
+                    )}
+                  </Flex>
+                </Stack>
+              );
+            })}
+            <Button variant="positive" onClick={handleAddClick}>
+              Add
+            </Button>
+          </Stack>
+        </Card>
       </Flex>
     </Flex>
   );
